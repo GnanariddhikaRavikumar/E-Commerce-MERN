94929f4b00fcef3dd8cf8b4e0d5a1f0c34bbe31e
Gnanariddhika <gnanariddhika@gmail.com>
2024-01-04 18:55:49 +0530

    second commit

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-//import Body from './Components/Body';
 import { Outlet } from 'react-router-dom';
 
 
